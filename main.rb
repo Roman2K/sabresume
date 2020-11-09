2da710461ecb55270bea6a9276e678be241323d3
Roman Le Négrate <roman.lenegrate@gmail.com>
2020-11-09 16:40:37 +0100

    only resume first paused

--- main.rb
+++ main.rb
@@ -8,12 +8,15 @@ sab = Utils::SABnzbd.new conf[:sab], log: log
 stats = sab.queue.group_by { |i| i.fetch "status" }
 pp stats: stats.transform_values(&:size)
 
-stats.delete("Paused") { [] }.each do |item|
-  log[filename: item.fetch("filename")].warn "resuming download"
-  sab.queue_resume item.fetch("nzo_id")
+if %w[Downloading Queued].sum { stats.fetch(_1, []).size }.zero?
+  log.warn "none downloading, resuming any paused download"
+  stats.fetch("Paused")[0,1].each do |item|
+    log[filename: item.fetch("filename")].info "resuming download"
+    sab.queue_resume item.fetch("nzo_id")
+  end
 end
 
-if stats.keys == ["Queued"]
+if stats.keys - %w[Paused Fetching] == ["Queued"]
   log.warn "all queued, resuming"
   sab.queue_resume_all
 end
