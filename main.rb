84f6afbaad3e12979167bb6352cf5d546fc49556
Roman Le Négrate <roman.lenegrate@gmail.com>
2020-11-18 13:11:18 +0100

    resume all paused downloads

--- main.rb
+++ main.rb
@@ -7,22 +7,19 @@ sab = Utils::SABnzbd.new conf[:sab], log: log
 
 stats = sab.queue.group_by { _1.fetch "status" }
 pp stats: stats.transform_values(&:size)
+item_log = -> item { log[filename: item.fetch("filename")] }
 
 stats.fetch("Downloading", []).each do |item|
   item.fetch("percentage").to_i > 0 && item.fetch("eta") == "unknown" or next
-  log[filename: item.fetch("filename")].
-    warn "download stuck, pausing and resuming"
+  item_log.(item).warn "download stuck, pausing and resuming"
   nzo_id = item.fetch "nzo_id"
   sab.queue_pause nzo_id
   sab.queue_resume nzo_id
 end
 
-if %w[Downloading Queued].sum { stats.fetch(_1, []).size }.zero?
-  log.warn "none downloading, resuming any paused download"
-  stats.fetch("Paused")[0,1].each do |item|
-    log[filename: item.fetch("filename")].info "resuming download"
-    sab.queue_resume item.fetch("nzo_id")
-  end
+stats.fetch("Paused", []).each do |item|
+  item_log.(item).warn "resuming download"
+  sab.queue_resume item.fetch("nzo_id")
 end
 
 if stats.keys - %w[Paused Fetching] == ["Queued"]
