87f56bf83434e5cdb6d42e5ccc784191cd2a20a8
Roman Le Négrate <roman.lenegrate@gmail.com>
2021-01-23 17:22:38 +0100

    try restarting, then deleting

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
+gem 'redis'
 gem 'utils-ruby', github: 'Roman2K/utils-ruby'
 # gem 'utils-ruby', path: '../utils-ruby'
--- main.rb
+++ main.rb
@@ -1,18 +1,46 @@
 require 'utils'
+require 'redis'
 
 conf = Utils::Conf.new("config.yml")
 log = Utils::Log.new level: :info
 log.level = :debug if ENV["DEBUG"] == "1"
 sab = Utils::SABnzbd.new conf[:sab], log: log
+redis = Redis.new url: conf[:redis]
 
 stats = sab.queue.group_by { _1.fetch "status" }
 pp stats: stats.transform_values(&:size)
 item_log = -> item { log[filename: item.fetch("filename")] }
 
+class FailureManager
+  def initialize(sab, redis)
+    @sab = sab
+    @redis = redis
+  end
+
+  def add_stuck(item, log:)
+    nzo_id = item.fetch "nzo_id"
+    key = "sabresume:strategies:#{nzo_id}"
+    if @redis.sadd(key, "restart")
+      @redis.expire key, 3600
+      log.info "restarting"
+      @sab.restart
+    elsif @redis.sadd(key, "delete")
+      log.info "deleting"
+      @sab.queue_del nzo_id, del_files: true
+      @redis.del key
+    else
+      log.error "already tried all strategies"
+    end
+  end
+end
+
+failmgr = FailureManager.new sab, redis
 stats.fetch("Downloading", []).each do |item|
+  pp item: item if item.fetch("filename") =~ /Insurg.*SPAR/
   if item.fetch("percentage").to_i > 0 && item.fetch("eta") == "unknown"
-    item_log.(item).warn "download stuck, restarting"
-    sab.restart
+    ilog = item_log.(item)
+    ilog.warn "download stuck"
+    failmgr.add_stuck item, log: ilog
     exit 0
   end
 end
