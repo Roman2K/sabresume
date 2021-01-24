63b51ae2775666ac0c240ec1d314234ab846f4e3
Roman Le Négrate <roman.lenegrate@gmail.com>
2021-01-24 01:33:38 +0100

    detect new case of stuck dl

--- main.rb
+++ main.rb
@@ -7,6 +7,28 @@ log.level = :debug if ENV["DEBUG"] == "1"
 sab = Utils::SABnzbd.new conf[:sab], log: log
 redis = Redis.new url: conf[:redis]
 
+class DryRunSAB < BasicObject
+  def initialize(sab, log:)
+    @sab = sab
+    @log = log
+  end
+
+  private def method_missing(m,*a,&b)
+    case m
+    when :history, :queue
+      @sab.public_send m,*a,&b
+    else
+      @log.info "sab.#{m}(#{o = a.shift and "#{o.inspect}, "}#{a.size} args...)"
+    end
+  end
+end
+if conf[:dry_run]
+  log.info "dry run"
+  sab = DryRunSAB.new sab, log: log["dry run"]
+else
+  log.info "real mode"
+end
+
 stats = sab.queue.group_by { _1.fetch "status" }
 pp stats: stats.transform_values(&:size)
 item_log = -> item { log[filename: item.fetch("filename")] }
@@ -36,13 +58,13 @@ end
 
 failmgr = FailureManager.new sab, redis
 stats.fetch("Downloading", []).each do |item|
-  pp item: item if item.fetch("filename") =~ /Insurg.*SPAR/
-  if item.fetch("percentage").to_i > 0 && item.fetch("eta") == "unknown"
-    ilog = item_log.(item)
-    ilog.warn "download stuck"
-    failmgr.add_stuck item, log: ilog
-    exit 0
-  end
+  next unless item.fetch("eta") == "unknown"
+  get_f = -> k { item.fetch(k).to_f }
+  next unless get_f["percentage"] > 0 || get_f["mbleft"] > get_f["mb"]
+  ilog = item_log.(item)
+  ilog.warn "download stuck"
+  failmgr.add_stuck item, log: ilog
+  exit 0
 end
 
 stats.fetch("Paused", []).each do |item|
